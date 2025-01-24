7f8203476f13685b8d080bdeebe141dbefb758a0
Juliette Bell <jbelllud@asu.edu>
2025-01-23 22:58:28 -0800

    Figuring it out and going to bed!

--- Squares/squaresPt2.js
+++ Squares/squaresPt2.js
@@ -14,10 +14,10 @@ function draw() {
     // First rectangle
 
     if (((mouseX > 100) && (mouseX < 175)) && ((mouseY > 250) && (mouseY < 325))) {
-        return lightA;
+        lightA = true;
     }
     else {
-        return false;
+        lightA = false;
     }
 
     if (lightA == true) {
@@ -31,6 +31,21 @@ function draw() {
     let rectA = rect(100, 250, 75, 75);
 
     // Second rectangle
+
+    if (((mouseX > 200) && (mouseX < 275)) && ((mouseY > 250) && (mouseY < 325)) && (lightA = true)) {
+        lightB = true;
+    }
+    else {
+        lightB = false;
+    }
+
+    if (lightB == true) {
+        fill(0, 255, 0);
+    }
+
+    else {
+        fill(255);
+    }
     let rectB = rect(200, 250, 75, 75);
 
     // Third Rectangle
